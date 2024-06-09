667107b6ab1a142cb698764ffb36bcd2a0792636
gopichand1939 <tummapalagopichand@gmail.com>
2024-06-09 04:35:07 +0000

    solution

---
+++ src/component/showGreetings/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './index.css'
+
+const ShowGreetings = props => {
+  const {languageDetails, updateButton, ActiveTab} = props
+
+  const {id, imageUrl, buttonText, imageAltText} = languageDetails
+  const activateBtn = ActiveTab ? 'active-btn' : ''
+
+  const sendActiveId = () => {
+    updateButton(id, imageUrl, imageAltText)
+  }
+
+  return (
+    <li>
+      <button
+        type="button"
+        className={`button ${activateBtn}`}
+        onClick={sendActiveId}
+      >
+        {buttonText}
+      </button>
+    </li>
+  )
+}
+
+export default ShowGreetings
